d86e02db60453420b90811df2a18d9406101a465
EyeZen <microshivam1320@gmail.com>
2022-04-12 21:15:37 -0400

    File Upload commit

--- bin/www.js
+++ bin/www.js
@@ -1,19 +1,17 @@
-// prod-deps:
-// npm i express mongoose
-
 const express = require("express");
-// const mongoose= require("mongoose");
+const multer = require('multer');
 
-// init: mongoose, needs the mongo server running
-// mongoose.connect('mongodb://localhost:27017/college')
-// const StudentSchema = new mongoose.Schema({
-//   name: String,
-//   collegeId: Number,
-//   class: String,
-//   section: String
-//   // img:?
-// });
-// const Student = mongoose.model("Student", StudentSchema);
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function(req, file, cb) {
+    filename = `${file.fieldname}-${Date.now()}.${file.mimetype.split('/')[1]}`;
+    console.log("Save File Name: " + filename);
+    cb(null, filename);
+  }
+});
+const upload = multer({storage});
 
 // init: express
 const app = express()
@@ -22,7 +20,7 @@ app.use((req,res, next)=>{
   next()
 })
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+// app.use(express.urlencoded({extended: false}))
 app.use(express.static("./public/Homepage"))
 
 app.listen(3000,()=>{
@@ -42,20 +40,13 @@ register.post("/", (req,res)=>{
     class: req.body.class,
     section: req.body.section
   }
-  // const student = new Student(stud);
-  
-  // todo: add student-data to db
-  // student.save()
-  // todo: train model on current db
   
   res.status(200).json(stud);
 })
 
 attendance.post("/",(req,res)=>{
   console.log("hit attendance")
-  const attendantsVideo = req.body.attendantsVideo;
-  // todo: use model here
-  // todo: get attendance list, from separate process(model)
+
   const attendanceList=[
     {name: 'sname', id: 1, class: 'sclass', section: 'ssectiion'},
     {name: 'sname', id: 2, class: 'sclass', section: 'ssectiion'},
@@ -70,7 +61,8 @@ attendance.use("/", express.static("./public/Mark_attendance/"))
 
 app.use('/register', register)
 app.use('/attendance', attendance)
-app.use('/video', (req, res)=>{
-  console.log({FileUploaded: req.body})
-  res.status(200).redirect("/attendance")
+app.use('/video',upload.single('video'), (req, res)=>{
+  console.log(req.file);
+  console.log(req.body);
+  return res.status(200).redirect("/attendance");
 })
